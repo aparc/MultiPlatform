f2d7cd4186203388e701a5c8cc8146a57aad9db3
Andrei <a.parchukov@yandex.ru>
2022-10-06 19:12:34 +0300

    Initial Commit

---
+++ Shared/MultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPlatformApp.swift
+//  Shared
+//
+//  Created by Андрей Парчуков on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
